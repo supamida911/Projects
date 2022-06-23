626385ebe75f8a2b79e74cc2845f3594ee4d9108
supamida911 <chester911@naver.com>
2022-06-24 00:25:26 +0900

    added

--- Olist/olist_2.sql
+++ Olist/olist_2.sql
@@ -41,3 +41,22 @@ from olist.orders as a
 where a.order_status not in ('unavailable', 'canceled')
 group by 1, 2
 order by 1;
+
+# 상품의 월별 지역별 주문량, 매출
+select d.product_category_name as 'product',
+	substr(a.order_purchase_timestamp, 1, 7) as YM,
+	b.customer_state as 'state',
+    count(distinct a.customer_id) as user_cnt,
+    sum(e.payment_value) as rev
+from olist.orders as a
+	left join olist.customers as b
+		on a.customer_id= b.customer_id
+	left join olist.order_items as c
+		on a.order_id= c.order_id
+	left join olist.products as d
+		on c.product_id= d.product_id
+	left join olist.payments as e
+		on a.order_id= e.order_id
+where a.order_status not in ('unavailable', 'canceled')
+group by 1, 2, 3
+order by 2;
